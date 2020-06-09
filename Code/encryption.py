b04fcafa31c3d785b5e17122886ec0099432935e
AkeelAtherMedina <59961447+AkeelAtherMedina@users.noreply.github.com>
2020-06-09 09:36:28 +0500

    Add files via upload
    circular queue helper functions added, dosent work tho idk i think its like a scope issue?? every time i enqueue front becomes -1 again help

--- Code/encryption.py
+++ Code/encryption.py
@@ -1,37 +1,79 @@
-def enqueue(lst, item):
-    lst.append(item)
+def enqueue(lst, item, front, rear, queuesize):
+    print(front)
+    if (rear+1) % queuesize == front:
+        print('Error: Queue is full')
+    elif front == -1:
+        front = 0
+        rear = 0
+        lst[rear]=item
+    else:
+        rear = (rear + 1) % queuesize
+        print(rear)
+        lst[rear]=item
+    print(front)
 
 
-def dequeue(lst):
-    return lst.pop(0)
+def dequeue(lst, front, rear, queuesize):
+    if front == -1:
+        print("Error: Queue is empty")
+    elif front == rear:
+        temp = lst[front]
+        front = -1
+        rear = -1
+        return temp
+    else:
+        temp = lst[front]
+        front = (front + 1) % queuesize
+        return temp
 
 
 def front(lst):
-    return lst[0]
+    return lst[front]
 
 
 def rear(lst):
-    return lst[-1]
+    return lst[rear]
 
 
-def is_empty(lst):
-    if len(lst) == 0:
-        return True
+def display(lst, front, rear, queuesize):
+    if front == -1:
+        print("Error: Queue is empty")
+    elif rear >= front:
+        print("Elements in the circular queue are:", end = " ")
+        for i in range(front, rear + 1):
+            print(lst[i], end = " ")
+        print()
     else:
-        return False
+        print ("Elements in the circular queue are:", end = " ")
+        for i in range(front, queuesize):
+            print(lst[i], end = " ")
+        for i in range(0, rear + 1):
+            print(lst[i], end = " ")
+        print()
+
+    if ((rear + 1) % queuesize == front):
+        print("Error: Queue is Full")
+
+
+
+    # def encryption(plaintext):
+#     cyphertext = ''
+#     asci = []
+#     for i in plaintext:
+#         asci.append(int(bin(ord(i))[0]+bin(ord(i))[2:]))
+#     return asci
 
 
 plaintext = input()
-
 queuesize = int(input())
+front = -1
+rear = -1
+queue = []
 
+for i in range(queuesize):
+    queue.append([])
 
+for i in range(len(plaintext)):
+    enqueue(queue, plaintext[i], front, rear, queuesize)
 
-def encryption(plaintext):
-    cyphertext = ''
-    asci = []
-    for i in plaintext:
-        asci.append(int(bin(ord(i))[0]+bin(ord(i))[2:]))
-    return asci
-
-print(encryption(plaintext))
+# print(encryption(plaintext))
