c3f80f7e9d0ac471b822b1d93db96cd997ed8a32
shayanshariff <60033950+shayanshariff@users.noreply.github.com>
2020-06-22 19:06:37 +0500

    gui stuff

--- Code/main.py
+++ Code/main.py
@@ -37,8 +37,56 @@ def dequeue(lst, front, rear, queuesize):
 # ok this is all thats important for u i think @shayan
 
 # the inputs
-plaintext = input()
-queuesize = int(input())
+import tkinter as gui
+
+
+def take_input(button_pressed): #this is whats called when the buttons are pressed, it takes the inputs and stores them in those variable
+    global plaintext
+    global queuesize 
+    plaintext = textbox.get()
+    queuesize = int(keybox.get())
+    if button_pressed == 'e': #this is to just decide if we wanna output the encrypted text or the decrypted text
+        outputbox['text'] = binary
+    elif button_pressed == 'd':
+        outputbox['text'] = plaintextoutput
+#the problem is binary and plaintextoutput both get defined at the end so thats the error we have to fix 
+
+    
+
+root = gui.Tk()
+
+canvas = gui.Canvas(root, height = 600, width = 800)
+canvas.pack()
+
+frame = gui.Frame(root, bg= '#aed6f1', bd=5)
+frame.place( relx = 0.5, rely = 0.1, relwidth= 0.75, relheight = 0.1, anchor='n')
+
+EncryptButton = gui.Button(frame, text= "Encrypt Text", command=lambda button_pressed ='e':  take_input(button_pressed))
+EncryptButton.place(relx = 0.7, relwidth =0.3, relheight= 0.5 )
+
+
+DecryptButton = gui.Button(frame, text= "Decrypt Text", command=lambda button_pressed ='d':  take_input(button_pressed))
+DecryptButton.place(relx = 0.7, rely =0.55 ,relwidth =0.3, relheight= 0.5 )
+
+textbox = gui.Entry(frame)
+textbox.place(relx = 0, rely = 0, relwidth= 0.65, relheight = 1)
+
+keyframe= gui.Frame(root, bg='#aed6f1', bd=4)
+keyframe.place(relx = 0.5, rely = 0.2 , relwidth = 0.75, relheight = 0.075, anchor='n')
+
+keybox = gui.Entry(keyframe)
+keybox.place(relx = 0, rely = 0, relwidth= 0.65, relheight = 1)
+
+
+result = gui.Frame(root,bg= '#aed6f1', bd=10)
+result.place(relx=0.5, rely= 0.25, relwidth =0.75, relheight = 0.6, anchor='n')
+
+outputbox = gui.Label(result, anchor="nw", justify='left', bd=4)
+outputbox.place(relwidth=1 ,relheight= 1)
+
+root.mainloop()
+
+
 
 # this isnt really important for gui but this initialises the queue
 if queuesize<len(plaintext):
