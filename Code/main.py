badf71254c59e01c7572129e652b400a4da78385
AkeelAtherMedina <59961447+AkeelAtherMedina@users.noreply.github.com>
2020-06-23 17:56:52 +0500

    Final Upload + gui fix
    fixed the gui finally

--- Code/main.py
+++ Code/main.py
@@ -3,7 +3,7 @@ from encryption import encryption
 from decryption import decryption
 global plaintext
 global queuesize
-global button 
+global button
 plaintext = ''
 queuesize = 0
 button = ''
@@ -47,58 +47,89 @@ while True:
     def take_input(strng):  # this is whats called when the buttons are pressed, it takes the inputs and stores them in those variable
         button = str(strng)
         plaintext = textbox.get()
-        queuesize = int(keybox.get())
-        if queuesize<len(plaintext):
-            while queuesize<len(plaintext):
-                print('The size of the queue has to be bigger than the input')
-                queuesize = int(input())
-        front = -1
-        rear = -1
-        queue = []
-        keyasci = []
-        letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-        if queuesize > 26:
-            x = queuesize-26
-        x = queuesize
-        for i in range(x):
-            letters.append('x')
-        # if the queue is longer than 26, it will just have x as the keyword
+        if button == 'e':
+            queuesize = int(keybox.get())
+            if queuesize<len(plaintext):
+                while queuesize<len(plaintext):
+                    print('The size of the queue has to be bigger than the input')
+                    queuesize = int(input())
+            front = -1
+            rear = -1
+            queue = []
+            keyasci = []
+            letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+            if queuesize > 26:
+                x = queuesize-26
+            x = queuesize
+            for i in range(x):
+                letters.append('x')
+            # if the queue is longer than 26, it will just have x as the keyword
 
-        for i in range(queuesize):
-            queue.append(['', letters[i]])
+            for i in range(queuesize):
+                queue.append(['', letters[i]])
 
-        # the initialisation is done, now its using the input to enqueue in the queue
+            # the initialisation is done, now its using the input to enqueue in the queue
 
-        for i in range(len(plaintext)):
-            front, rear = enqueue(queue, plaintext[i], front, rear, queuesize)
-        # using the queue for encryption
-        keywords = [i[1] for i in queue]
-        for i in keywords:
-            keyasci.append(bin(ord(i))[0] + bin(ord(i))[2:])
-
-
-        ''''''
-        binary = encryption(plaintext, keyasci)
-
-        word = decryption(binary, keyasci)
-
-        plaintextoutput = ''
-        for i in word:
-            plaintextoutput+=i
+            for i in range(len(plaintext)):
+                front, rear = enqueue(queue, plaintext[i], front, rear, queuesize)
+            # using the queue for encryption
+            keywords = [i[1] for i in queue]
+            for i in keywords:
+                keyasci.append(bin(ord(i))[0] + bin(ord(i))[2:])
 
 
-        for i in range(len(plaintext)):
-            front, rear = dequeue(queue, front, rear, queuesize)
-        output = ""
+            ''''''
+            binary = encryption(plaintext, keyasci)
 
-        if button == 'e':  # this is to just decide if we wanna output the encrypted text or the decrypted text
             output = binary
+
+            outputbox['text'] = output
+
         elif button == 'd':
+            plaintext = plaintext.split()
+            queuesize = int(keybox.get())
+
+            front = -1
+            rear = -1
+            queue = []
+            keyasci = []
+            letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                       't', 'u', 'v', 'w', 'x', 'y', 'z']
+            if queuesize > 26:
+                x = queuesize - 26
+            x = queuesize
+            for i in range(x):
+                letters.append('x')
+            # if the queue is longer than 26, it will just have x as the keyword
+
+            for i in range(queuesize):
+                queue.append(['', letters[i]])
+
+            # the initialisation is done, now its using the input to enqueue in the queue
+
+            for i in range(len(plaintext)):
+                front, rear = enqueue(queue, plaintext[i], front, rear, queuesize)
+            # using the queue for encryption
+            keywords = [i[1] for i in queue]
+            for i in keywords:
+                keyasci.append(bin(ord(i))[0] + bin(ord(i))[2:])
+
+            ''''''
+
+            word = decryption(plaintext, keyasci)
+
+            plaintextoutput = ''
+            for i in word:
+                plaintextoutput += i
+
+            for i in range(len(plaintext)):
+                front, rear = dequeue(queue, front, rear, queuesize)
+            output = ""
+
             output = plaintextoutput
 
-        outputbox['text'] = output
+            outputbox['text'] = output
 
-    # the problem is binary and plaintextoutput both get defined at the end so thats the error we have to fix
 
 
 
@@ -133,10 +164,3 @@ while True:
 
     root.mainloop()
 
-
-
-
-
-# if you want the encrypted output, the variable called binary has it
-# ok finally plaintextoutput is the final answer, which should be the exact same as the input called plaintext
-# the rest of the work dosent matter for gui i think, it all works using the encryption and decryption function
